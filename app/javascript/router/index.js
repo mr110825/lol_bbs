3e4d536cf751f66331492f137adb0d9ef021ed47
mr110825 <ptin110825@gmail.com>
2021-11-26 19:55:48 +0900

    feat:トップページを実装、画面遷移機能及び非常に簡易な表示

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -1,2 +1,18 @@
 import Vue from 'vue';
 import Router from "vue-router";
+
+import HomePage from "../pages/home/homepage.vue";
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+      name: "HomePage",
+    }
+  ],
+})
+
+export default router
