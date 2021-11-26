34dfea4ec759bffa901891cf0bbb85df9bd5b563
mr110825 <ptin110825@gmail.com>
2021-11-26 21:50:17 +0900

    feat:ナビバー実装

--- app/javascript/pages/home/homepage.vue
+++ app/javascript/pages/home/homepage.vue
@@ -1,25 +1,38 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-    <div class="text-center my-3">
-      <b-button v-b-tooltip.hover title="Tooltip content">Hover Me</b-button>
-    </div>
-  </div>
+<div class="navigation">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">{{ message }}</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Article</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Search</a>
+          </li>
+                  <li class="nav-item">
+            <a class="nav-link" href="#">Sign in</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+  <h1>{{ description }}</h1>
+</div>
 </template>
 
 <script>
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "LOL-BBS",
+      description: "アプリの説明"
     }
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+.navigation{
+  background-color:#fca311;
 }
 </style>
